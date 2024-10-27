bebd83b3b2ff55173bae804f7c35260fd37d8103
100472104 <95222796+Luis5707@users.noreply.github.com>
2024-10-27 10:34:25 +0100

    Update cleanData.py
    Actualizo

--- cleanData.py
+++ cleanData.py
@@ -5,36 +5,80 @@ from datetime import datetime
 import unicodedata
 import re
 
-from enumIincidencias import IncidenciaUsuario
 
 #TODO Variables para las clases de cada base
 
-CLASES_AREA = ["ID", "LATITUD", "LONGITUD", "BARRIO", "DISTRITO", "ESTADO", "FECHA_INSTALACION", "TOTAL_ELEM"]
-CLASE_SATISFACCION = ["ID", "FECHA","PUNTUACION_ACCESIBILIDAD", "PUNTUACION_CALIDAD", "COMENTARIOS"]
-CLASE_USUARIO = ["NIF", "NOMBRE", "EMAIL", "TELEFONO"]
-CLASE_INDICENDIA_USUARIO = ["ID", "TIPO_INCIDENCIA", "FECHA_REPORTE", "ESTADO"] #TODO FALTA TIEMPO RESOLUCION
+#CLASES_AREA = ["ID", "LATITUD", "LONGITUD", "BARRIO", "DISTRITO", "ESTADO", "FECHA_INSTALACION", "TOTAL_ELEM"]
+#CLASE_SATISFACCION = ["ID", "FECHA","PUNTUACION_ACCESIBILIDAD", "PUNTUACION_CALIDAD", "COMENTARIOS"]
+#CLASE_USUARIO = ["NIF", "NOMBRE", "EMAIL", "TELEFONO"]
+#CLASE_INCICENDIA_USUARIO = ["ID", "TIPO_INCIDENCIA", "FECHA_REPORTE", "ESTADO"] # TODO FALTA TIEMPO RESOLUCION
+#CLASE_INCIDENCIA_SEGURIDAD = ["ID", "FECHA_REPORTE", "TIPO_INCIDENTE", "GRAVEDAD"]
+#CLASE_JUEGO = ["ID","DESC_CLASIFICACION", "MODELO", "ESTADO", "ACCESIBLE", "FECHA_INSTALACION", "TIPO_JUEGO", "FECHA_INTERVENCION"]# TODO desgasteAcumulado, indicadorExposicion no sabemos-- suponemos ultimaFechaMantenimiento de Mantenimiento
+#CLASE_MANTENIMIENTO = ["ID", "FECHA_INTERVENCION", "TIPO_INTERVENCION", "ESTADO_PREVIO", "ESTADO_POSTERIOR"]
+##CLASE_REGISTRO_CLIMA = [""] TODO -- No sabemos que poner aqui
 
+NOMBRES_ARCHIVOS = {"AreasSucio.csv":
+                        ["ID", "COORDENADAS", "BARRIO", "DISTRITO", "ESTADO", "FECHA_INSTALACION",
+                                      "TOTAL_ELEM"],
+                    "EncuestasSatisfaccionSucio.csv":[
+                        "ID", "FECHA","PUNTUACION_ACCESIBILIDAD", "PUNTUACION_CALIDAD", "COMENTARIOS"],
+                    "IncidenciasUsuariosSucio.csv": ["ID", "TIPO_INCIDENCIA", "FECHA_REPORTE", "ESTADO"],
+                    "IncidentesSeguridadSucio.csv": ["ID", "FECHA_REPORTE", "TIPO_INCIDENTE", "GRAVEDAD"],
+                    "JuegosSucio.csv": ["ID","DESC_CLASIFICACION", "MODELO", "ESTADO", "ACCESIBLE", "FECHA_INSTALACION",
+                                       "tipo_juego" ], # TODO agregar "FECHA_INTERVENCION"
+                    "MantenimientoSucio.csv": ["ID", "FECHA_INTERVENCION", "TIPO_INTERVENCION", "ESTADO_PREVIO",
+                                               "ESTADO_POSTERIOR"],
+                    "meteo24.csv": ["PROVINCIA", "MUNICIPIO"],
+                    "UsuariosSucio.csv": ["NIF", "NOMBRE", "EMAIL", "TELEFONO"]
+                    }
 
 
 class CSVProcessor:
     def __init__(self, directorio):
         self.directorio = directorio
-        self.enumIndicencias = IncidenciaUsuario  # Instancia de EnumValuesIncidencias
         self.ruta_archivo = None
-    
+
+
+    def check_coordenates(self, latitud, longitud):
+        """Comprobar el formato de """
+
+
+    def delete_columns(self, df):
+        """Funcion que elimina las columnas innecesarias de los archivos csv"""
+
+        for nombre in NOMBRES_ARCHIVOS.keys():
+            if nombre in self.ruta_archivo:
+
+                # Considerar las columnas relevantes
+
+                # En caso de ser el archivo de areas, crear el atributo coordenadas
+                if nombre == "AreasSucio.csv":
+                    if 'LATITUD' in df.columns and 'LONGITUD' in df.columns:
+                        try:
+                            df.loc[:, 'COORDENADAS'] = df['LATITUD'].astype(str) + ', ' + df['LONGITUD'].astype(str)
+                            df = df.drop(['LATITUD', 'LONGITUD'], axis=1)
+                        except Exception:
+                            pass
+
+                df = df[NOMBRES_ARCHIVOS[nombre]]
+
+                # Devolver el datagrama
+                return df
+
+        return df
+
+
     def cargar_y_reemplazar_csv(self, ruta_archivo):
         self.ruta_archivo = ruta_archivo        # Actualizarlo segun el archivo
         df = pd.read_csv(ruta_archivo)
+
+        # Borrar informacion innecesaria
+        df = self.delete_columns(df)
 
         for columna in df.columns:
             df[columna] = df[columna].map(
                 lambda x: 'None' if pd.isna(x) or str(x).strip() == '' else self.corregir_tipografia(x))
 
-
-        #if any(col.lower() == 'telefono' for col in df.columns):
-        #    df['teléfono'] = df['teléfono'].apply(self.formatear_telefono(x))
-
-        for columna in df.columns:
             if 'fecha' in columna.lower():
                 try:
                     df[columna] = df[columna].map(lambda x: self.corregir_fecha(x))
@@ -45,10 +89,8 @@ class CSVProcessor:
             elif 'telefono' in columna.lower():
                 df[columna] = df[columna].map(lambda x: self.formatear_telefono(x))
 
-            elif 'cod_postal' in columna.lower():
-                df[columna] = df[columna].map(lambda x: self.corregir_cod_postal(x))
 
-        # Quitar los duplicados
+        # Quitar las instancias duplicadas
         if 'ID' in df.columns:
             df = df.drop_duplicates(subset='ID', keep='first')
             print("Duplicados en la columna 'ID' eliminados.")
@@ -57,18 +99,20 @@ class CSVProcessor:
         print(
             f"Archivo '{ruta_archivo}' procesado y guardado con valores nulos, fechas estandarizadas y errores tipográficos corregidos.")
 
+
+
     def corregir_fecha(self, fecha):
         if fecha == 'None':
             return 'None'
 
         try:
             return pd.to_datetime(fecha, errors='raise').isoformat()
-        except:
+        except UserWarning:
             pass
 
         try:
             return pd.to_datetime(fecha, format='%d/%m/%Y', errors='raise').isoformat()
-        except:
+        except UserWarning:
             pass
 
         return 'None'
@@ -84,12 +128,6 @@ class CSVProcessor:
             # Si el número no tiene el formato adecuado, retornarlo tal cual
             return numero
 
-    def corregir_cod_postal(self, texto):
-        if texto != None:
-            return str(texto)[:-2]
-        else:
-            return texto
-
     def corregir_tipografia(self, texto):
 
         #TODO ejeplo para un tipo de archivo
@@ -103,14 +141,6 @@ class CSVProcessor:
             return texto
         return texto
 
-    def corregir_enum_incidencias(self, valor, valores_permitidos):
-
-        if valor in valores_permitidos:
-            return valor
-        else:
-            coincidencias = get_close_matches(valor, valores_permitidos, n=1, cutoff=0.6)
-            return coincidencias[0] if coincidencias else 'None'
-
     def procesar_archivos_csv(self):
         for archivo in os.listdir(self.directorio):
             if archivo.endswith('.csv'):
@@ -119,7 +149,7 @@ class CSVProcessor:
 
 
 # Directorio donde están los archivos .csv
-directorio = '/home/luis/Escritorio/practica1_2/Datasets Práctica 1.2-20241015'
+directorio = './Datasets Práctica 1.2-20241015'
 
 # Crear una instancia de la clase CSVProcessor
 procesador_csv = CSVProcessor(directorio)
