3a0d9c30e4a6d13878c14d906abd890725a87590
100472104 <95222796+Luis5707@users.noreply.github.com>
2024-11-09 17:13:30 +0100

    Delete importaciones.js

--- importaciones.js
+++
@@ -1,9 +0,0 @@
-mongoimport --db parque_recreativo --collection areaRecreativa --type csv --file Datasets_Practica_1.2/AreasSucio.csv --headerline;
-mongoimport --db parque_recreativo --collection encuestaSatisfaccion --type csv --headerline --file Datasets_Practica_1.2/EncuestasSatisfaccionSucio.csv;
-mongoimport --db parque_recreativo --collection incidencia --type csv --headerline --file Datasets_Practica_1.2/IncidenciasUsuariosSucio.csv;
-mongoimport --db parque_recreativo --collection incidenteSeguridad --type csv --headerline --file Datasets_Practica_1.2/IncidentesSeguridadSucio.csv;
-mongoimport --db parque_recreativo --collection usuario --type csv --headerline --file Datasets_Practica_1.2/UsuariosSucio.csv;
-mongoimport --db parque_recreativo --collection juego --type csv --headerline --file Datasets_Practica_1.2/JuegosSucio.csv;
-mongoimport --db parque_recreativo --collection mantenimiento --type csv --headerline --file Datasets_Practica_1.2/MantenimientoSucio.csv;
-mongoimport --db parque_recreativo --collection registroClima --type csv --headerline --file Datasets_Practica_1.2/MeteoModified.csv;
-mongoimport --db parque_recreativo --collection estaciones --type csv --headerline --file Datasets_Practica_1.2/estaciones_meteo_CodigoPostal.csv;
